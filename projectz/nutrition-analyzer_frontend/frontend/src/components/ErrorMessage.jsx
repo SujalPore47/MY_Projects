63b5c980fd77f3d541b06ec0e4f1d1bad276cd64
SujalPore47 <148222905+SujalPore47@users.noreply.github.com>
2025-04-20 19:30:01 +0530

    Delete projectz directory

--- projectz/nutrition-analyzer_frontend/frontend/src/components/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/ErrorMessage.jsx
-import React from 'react';
-import { Alert } from '@mui/material';
-
-function ErrorMessage({ error }) {
-  return error ? <Alert severity="error" sx={{ mt: 2 }}>{error}</Alert> : null;
-}
-
-export default ErrorMessage;
--- projectz/nutrition-analyzer_frontend/frontend/src/components/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { CircularProgress } from '@mui/material';
-
-function LoadingSpinner() {
-  return <CircularProgress aria-label="Loading" />;
-}
-
-export default LoadingSpinner;
